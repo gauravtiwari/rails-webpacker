b94c6612869cc56531fbd7256280e9d519bba1e2
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2016-12-18 17:34:16 +0000

    Add routes for vue powered views

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
+  get '/home-vue-version', to: 'pages#home_vue', as: :home_vue
+  get '/list-vue-version', to: 'lists#index_vue', as: :lists_vue
   resources :lists
 end
