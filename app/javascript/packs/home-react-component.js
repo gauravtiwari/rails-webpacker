5f00e2ba75c40923c4ac9c783ee3062de1c23199
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2016-12-19 11:43:22 +0000

    Rename for clarity

--- app/javascript/packs/home-react-component.js
+++ app/javascript/packs/home-react-component.js
@@ -1,6 +1,6 @@
 import ReactDOM from 'react-dom';
 import React from 'react';
 import { mount, withOptions } from 'react-mounter';
-import Home from './home';
+import Home from './home-react';
 
 mount(Home);
--- app/javascript/packs/list-react-component.js
+++ app/javascript/packs/list-react-component.js
@@ -1,6 +1,6 @@
 import ReactDOM from 'react-dom';
 import React from 'react';
 import { mount, withOptions } from 'react-mounter';
-import List from './list';
+import List from './list-react';
 
 mount(List);
