eb9c4b74ee1c1acd3f191120f511ee014556dac9
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2016-12-20 06:32:32 +0000

    Add action and routes for client side router pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -7,4 +7,13 @@ class PagesController < ApplicationController
 
   def home_angular
   end
+
+  def home_react_router
+  end
+
+  def about
+  end
+
+  def repos
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,8 @@ Rails.application.routes.draw do
   get '/home-vue-version', to: 'pages#home_vue', as: :home_vue
   get '/list-vue-version', to: 'lists#index_vue', as: :lists_vue
   get '/home-angular2-version', to: 'pages#home_angular', as: :home_angular
+  get '/home-react-router', to: 'pages#home_react_router', as: :home_router
+  get '/about', to: 'pages#about', as: :about
+  get '/repos', to: 'pages#repos', as: :repos
   resources :lists
 end
