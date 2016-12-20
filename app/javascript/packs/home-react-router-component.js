eb7ef5070b69251a8167425fbb308d4eb0c08bca
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2016-12-20 06:36:59 +0000

    Rename component names for clarity

--- app/javascript/packs/home-react-router-component.js
+++ app/javascript/packs/home-react-router-component.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import Home from './home-react';
-import App from './pages-react-router/app';
-import About from './pages-react-router/about'
-import Repos from './pages-react-router/repos'
+import App from './home-react-router/app';
+import About from './home-react-router/about'
+import Repos from './home-react-router/repos'
 import { render } from 'react-dom';
 import { Router, Route, hashHistory } from 'react-router'
 
