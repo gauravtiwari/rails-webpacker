fd2cc0fd766dc436213d30811134d8da63bc3e14
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2016-12-20 07:40:17 +0000

    Fix angular nav style

--- app/javascript/packs/home-angular2/index.js
+++ app/javascript/packs/home-angular2/index.js
@@ -19,21 +19,23 @@ HomeComponent.annotations = [
                 <aside>
                   <h1>{{ sidebar }}</h1>
                 </aside>
-                <a
-                  class="block"
-                  href="${Routes.home_vue_path()}"
-                >
-                  Home page(vue version)
-                </a>
-                <a class="block" href="${Routes.lists_path()}">
-                  Lists page (react)
-                </a>
-                <a class="block" href="${Routes.lists_vue_path()}">
-                  Lists page (vue)
-                </a>
-                <a class="block" href="${Routes.home_router_path()}">
-                  App page (react-router)
-                </a>
+                <div class="demos-nav">
+                  <a
+                    class="block"
+                    href="${Routes.home_vue_path()}"
+                  >
+                    Home page(vue version)
+                  </a>
+                  <a class="block" href="${Routes.lists_path()}">
+                    Lists page (react)
+                  </a>
+                  <a class="block" href="${Routes.lists_vue_path()}">
+                    Lists page (vue)
+                  </a>
+                  <a class="block" href="${Routes.home_router_path()}">
+                    App page (react-router)
+                  </a>
+                </div>
               </main>`
   })
 ];
