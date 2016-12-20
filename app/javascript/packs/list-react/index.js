c7b7700a032aa3ac01e2641c4ce495b6003ac59a
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2016-12-20 07:13:23 +0000

    Add example for using sass within components

--- app/javascript/packs/list-react/index.js
+++ app/javascript/packs/list-react/index.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react';
 import SharedNav from '../shared/nav';
 import Item from './item';
 
+// Component Styles
+import './styles/list.sass';
+
 export default class List extends Component {
   constructor(props) {
     super(props);
