122bbe2cef2c83a11adc2ab81425b5ed8c3d816c
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2017-01-21 15:35:05 +0000

    Cleanup comment

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,5 @@
 # app/models/user.rb
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   has_many :messages
