068e2b346d4bb02892dfd19a4d59362305d496b7
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2017-01-21 15:55:40 +0000

    Use functional component for message

--- app/javascript/packs/chat/message.js
+++ app/javascript/packs/chat/message.js
@@ -1,24 +1,19 @@
 import React, { Component } from 'react';
 
-export default class Message extends Component {
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    const { message } = this.props;
-    return(
-      <li className="message-list-item">
-        <h5 className="message-author-name">{message.user.name}</h5>
-        <div className="message-time">
-          {message.created_at}
-        </div>
-        <div className="message-text">{message.body}</div>
-      </li>
-    );
-  }
-}
+const Message = (props) => (
+  <li className="message-list-item">
+    <h5 className="message-author-name">
+      {props.message.user.name}
+    </h5>
+    <div className="message-time">
+      {props.message.created_at}
+    </div>
+    <div className="message-text">{props.message.body}</div>
+  </li>
+);
 
 Message.propTypes = {
   message: React.PropTypes.object,
 };
+
+export default Message;
