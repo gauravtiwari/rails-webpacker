935e96677a6e0c7688a01cd82e09a4f92a881c2f
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2017-01-21 15:21:43 +0000

    Add actioncable config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -39,8 +39,12 @@ Rails.application.configure do
 
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
-  # config.action_cable.url = 'wss://example.com/cable'
-  # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
+
+  config.action_cable.url = 'wss://rails-webpacker.herokuapp.com/cable'
+  config.action_cable.allowed_request_origins = [
+    'https://rails-webpacker.herokuapp.com',
+    'http://rails-webpacker.herokuapp.com'
+  ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
