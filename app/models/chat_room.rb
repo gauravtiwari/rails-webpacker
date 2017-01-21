f2090ef46460c22a440e938dd49a3a0697636ff1
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2017-01-21 15:30:36 +0000

    Cleanup old messages and rooms

--- app/models/chat_room.rb
+++ app/models/chat_room.rb
@@ -4,10 +4,15 @@ class ChatRoom < ApplicationRecord
   has_many :users, through: :messages
   validates :topic, presence: true, uniqueness: true, case_sensitive: false
   before_validation :add_slug
+  after_commit :cleanup_old_chat_rooms, on: :create
 
   private
 
   def add_slug
     self.slug = topic.parameterize
+  end
+
+  def cleanup_old_searches
+    ChatRoom.order(id: :desc).offset(20).destroy_all
   end
 end
--- app/models/message.rb
+++ app/models/message.rb
@@ -2,4 +2,11 @@
 class Message < ApplicationRecord
   belongs_to :chat_room
   belongs_to :user
+  after_commit :cleanup_old_messages, on: :create
+
+  private
+
+  def cleanup_old_searches
+    Message.order(id: :desc).offset(20).destroy_all
+  end
 end
