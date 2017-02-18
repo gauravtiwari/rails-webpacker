8aa5d4921984f21509c4da787dbb0c166b13acd4
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2017-02-18 06:55:19 +0000

    Add packs_dist_path config

--- config/application.rb
+++ config/application.rb
@@ -8,8 +8,6 @@ Bundler.require(*Rails.groups)
 
 module RailsWebpacker
   class Application < Rails::Application
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
+    config.x.webpacker[:packs_dist_path] = 'packs'
   end
 end
