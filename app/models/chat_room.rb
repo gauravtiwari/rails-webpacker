fea88e946e29b1519edd0cc2f755299e52b7a6c2
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2017-03-18 08:50:03 +0000

    Only allow english words

--- app/models/chat_room.rb
+++ app/models/chat_room.rb
@@ -3,6 +3,7 @@ class ChatRoom < ApplicationRecord
   has_many :messages, dependent: :destroy
   has_many :users, through: :messages
   validates :topic, presence: true, uniqueness: true, case_sensitive: false
+  validates_format_of :topic, with: /^[A-Za-z0-9 ]*$/
   before_validation :add_slug
   after_commit :cleanup_old_chat_rooms, on: :create
 
